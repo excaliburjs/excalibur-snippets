a10b2d2a6311415ea1ce52a3f44830f5a6654df9
Erik Onarheim <erik.onarheim@gmail.com>
2021-10-02 07:36:31 -0500

    add display mode snippets

---
+++ src/displaymode/fitcontainer/main.ts
@@ -0,0 +1,19 @@
+import * as ex from 'excalibur';
+import playerSrc from './player.png';
+// start-snippet{displaymode.fitcontainer}
+const game = new ex.Engine({
+    canvasElementId: 'game',
+    width: 600,
+    height: 400,
+    displayMode: ex.DisplayMode.FitContainer // highlight-line
+});
+// end-snippet{displaymode.fitcontainer}
+const playerImage = new ex.ImageSource(playerSrc);
+const loader = new ex.Loader([playerImage]);
+game.start(loader).then(() => {
+    const actor = new ex.Actor({
+        pos: game.screen.center
+    });
+    actor.graphics.use(playerImage.toSprite());
+    game.currentScene.add(actor);
+});
---
+++ src/displaymode/fitscreen/main.ts
@@ -0,0 +1,18 @@
+import * as ex from 'excalibur';
+import playerSrc from './player.png';
+// start-snippet{displaymode.fitscreen}
+const game = new ex.Engine({
+    width: 600,
+    height: 400,
+    displayMode: ex.DisplayMode.FitScreen // highlight-line
+});
+// end-snippet{displaymode.fitscreen}
+const playerImage = new ex.ImageSource(playerSrc);
+const loader = new ex.Loader([playerImage]);
+game.start(loader).then(() => {
+    const actor = new ex.Actor({
+        pos: game.screen.center
+    });
+    actor.graphics.use(playerImage.toSprite());
+    game.currentScene.add(actor);
+});
