827b8d953a784e779a37bc75e016947059b80da6
Erik Onarheim <erik.onarheim@gmail.com>
2025-10-19 11:13:07 -0500

    update snippets

--- src/spritesheet/main.ts
+++ src/spritesheet/main.ts
@@ -1,11 +1,12 @@
 import * as ex from 'excalibur';
-import { vec } from 'excalibur';
+import { ExcaliburGraphicsContext } from 'excalibur';
 
 import kennyCardsImageSrc from './kenny-cards.png';
 
 const game = new ex.Engine({
     width: 600,
     height: 400,
+    pixelArt: true,
     displayMode: ex.DisplayMode.FitScreen
 });
 
@@ -27,34 +28,50 @@ const spriteSheet = ex.SpriteSheet.fromImageSource({
         margin: { x: 23, y: 5}
     }
 });
+
+enum Suit {
+	Hearts = 0,
+	Diamonds = 1,
+	Clubs = 2,
+	Spades = 3
+}
+
+enum Rank {
+	Ace = 0,
+	Two = 1,
+	Three = 2,
+	Four = 3,
+	Five = 4,
+	Six = 5,
+	Seven = 6,
+	Eight = 7,
+	Nine = 8,
+	Ten = 9,
+	Jack = 10,
+	Queen = 11,
+	King = 12
+}
+
+
 // end-snippet{spritesheet}
 const animation = new ex.Animation({
     frames: spriteSheet.sprites.map(s => ({ graphic: s, duration: 300})),
     strategy: ex.AnimationStrategy.PingPong
 });
 
-const animation2 = ex.Animation.fromSpriteSheet(
-    spriteSheet,
-    ex.range(0, spriteSheet.sprites.length-1),
-    300,
-    ex.AnimationStrategy.PingPong
-);
-
 const actor = new ex.Actor({
-    pos: ex.vec(game.halfDrawWidth, game.halfDrawHeight)
+    pos: ex.vec(game.halfDrawWidth, game.halfDrawHeight),
+    scale: ex.vec(2, 2)
 });
-actor.graphics.show(animation, {
-    offset: vec(-50,0),
-});
-actor.graphics.show(animation2, {
-    offset: vec(50, 0)
-});
+actor.graphics.use(animation);
 
-console.log(game.currentScene.camera.pos);
+game.currentScene.onPostDraw = (ctx: ExcaliburGraphicsContext) => {
+	spriteSheet.getSprite(Rank.Ten, Suit.Spades).draw(ctx, actor.pos.x+100, actor.pos.y);
+
+	spriteSheet.getSprite(Rank.Ten, Suit.Hearts).draw(ctx, actor.pos.x-100, actor.pos.y);
+}
+
 const loader = new ex.Loader([kennyCardsImage]);
 game.start(loader).then(() => {
-    game.currentScene.camera.zoom = 2;
-    game.currentScene.camera.pos = ex.vec(game.halfDrawWidth, game.halfDrawHeight);
-    console.log(game.currentScene.camera.pos);
     game.currentScene.add(actor);
 });
--- src/tiled/main.ts
+++ src/tiled/main.ts
@@ -7,23 +7,29 @@ import tileset from './assets/kenny-rpg-urban-pack/tilemap_packed.png';
 const game = new ex.Engine({
     width: 600,
     height: 400,
+    pixelArt: true,
     displayMode: ex.DisplayMode.FitScreen
 });
 
-const tiledMapResource = new tiled.TiledMapResource(exampleCityUrl);
+const tiledMapResource = new tiled.TiledResource(exampleCityUrl, {
+	pathMap: [
+		{ path: 'tilemap_packed.png', output: tileset }
+	]
+
+});
 
 // Only necessary for parcel v2 rearranging assets at the root
 // or if you have a build system that moves resources linked by the .tmx
-tiledMapResource.convertPath = (tmxLocation, relativePath) => {
-    const resourceName = relativePath.split('/').at(-1)?.split('.')[0];
-    // for each linked tileset
-    if (tileset.includes(resourceName)) {
-        return tileset;
-    }
-}
+// tiledMapResource.pathMap.convertPath = (tmxLocation, relativePath) => {
+//     const resourceName = relativePath.split('/').at(-1)?.split('.')[0];
+//     // for each linked tileset
+//     if (tileset.includes(resourceName)) {
+//         return tileset;
+//     }
+// }
 const loader = new ex.Loader([tiledMapResource])
 game.start(loader).then(() => {
-    tiledMapResource.addTiledMapToScene(game.currentScene);
+    tiledMapResource.addToScene(game.currentScene);
     game.currentScene.camera.zoom = 2;
 });
 // end-snippet{tiled}
